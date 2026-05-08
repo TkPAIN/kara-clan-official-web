23a266f1a31d65d5bd20886f9a882df5141f91a7
TkPAIN <129594118+TkPAIN@users.noreply.github.com>
2026-05-08 20:14:36 +0200

    Update PowerShell command URL in Revert_Optimizer.bat

--- Revert_Optimizer.bat
+++ Revert_Optimizer.bat
@@ -1,5 +1,5 @@
 @echo off
 title Kara Clan - Revert Optimizer
 echo Downloading and running Kara Clan Revert Optimizer...
-powershell -NoProfile -ExecutionPolicy Bypass -Command "& { $s = Invoke-RestMethod 'https://raw.githubusercontent.com/TkPAIN/kara-clan-win-op/main/Revert_Optimizer.ps1'; Invoke-Expression $s }"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& { $s = Invoke-RestMethod 'https://raw.github.com/TkPAIN/kara-clan-official-web/blob/main/Revert_Optimizer.ps1'; Invoke-Expression $s }"
 pause
