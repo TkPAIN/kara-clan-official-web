3bfc3729151b75a8618a5fbf3f670a911825ea06
TkPAIN <129594118+TkPAIN@users.noreply.github.com>
2026-05-08 20:12:07 +0200

    Update Optimizar_v1.bat

--- Optimizar_v1.bat
+++ Optimizar_v1.bat
@@ -1,5 +1,6 @@
 @echo off
 title Kara Clan - Optimizer Vol.1
-echo Downloading and running Kara Clan Optimizer Vol.1...
+echo Kara Clan Optimizer Vol.1 — Essential
+echo Downloading and running...
 powershell -NoProfile -ExecutionPolicy Bypass -Command "& { $s = Invoke-RestMethod 'https://raw.githubusercontent.com/TkPAIN/kara-clan-win-op/main/OptimizarWindows_v1.ps1'; Invoke-Expression $s }"
 pause
