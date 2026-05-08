80591c3cf19aa93a755c3ff943bce3daa9a8a5bd
TkPAIN <129594118+TkPAIN@users.noreply.github.com>
2026-05-08 21:28:19 +0200

    Update PowerShell command URL in batch file

--- Optimizar_v3.bat
+++ Optimizar_v3.bat
@@ -3,5 +3,5 @@ title Kara Clan - Optimizer Vol.3
 echo Kara Clan Optimizer Vol.3 — Complete
 echo.
 echo Downloading and running...
-powershell -NoProfile -ExecutionPolicy Bypass -Command "& { $s = Invoke-RestMethod 'https://raw.githubusercontent.com/TkPAIN/kara-clan-win-op/main/OptimizarWindows_v3.ps1'; Invoke-Expression $s }"
+powershell -NoProfile -ExecutionPolicy Bypass -Command "& { $s = Invoke-RestMethod 'https://raw.githubusercontent.com/TkPAIN/kara-clan-official-web/main/OptimizarWindows_v3.ps1'; Invoke-Expression $s }"
 pause
